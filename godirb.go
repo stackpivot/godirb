7c58535a3c90af7702285a240d553402a83bccc9
Jon <git@alleskaputt.com>
2018-04-29 12:59:25 +0400

    removed cases from robots

--- godirb.go
+++ godirb.go
@@ -14,7 +14,7 @@ import (
 var result_slice []string
 
 
-func bruteforce_url(url_c chan string,  {
+func bruteforce_url(url_c chan string)  {
 
    for{
       url := <- url_c
@@ -27,19 +27,19 @@ func bruteforce_url(url_c chan string,  {
 
          // 401 unauthorized
          if status_code == 401{
-            result_slice = append(result_slicem, url)
-         }
+            result_slice = append(result_slice, url)
 
          // 200 Found
          } else if status_code == 200{
-            result_slice = append(result_slicem, url)
+            result_slice = append(result_slice, url)
 
 
          // 403 Forbidden
          } else if status_code == 403{
 
-            result_slice = append(result_slicem, url)
+            result_slice = append(result_slice, url)
          }
+      }
    }
 }
 
@@ -48,7 +48,8 @@ func bruteforce_url(url_c chan string,  {
 // gets robots.txt entires, returns string array with identified ressources
 func getRobots(url string) []string {
 
-      robots_url := url
+
+   robots_url := url
    if last_char := url[len(url)-1:]; last_char == "/"{
       robots_url = robots_url + "robots.txt"
    } else{
@@ -60,18 +61,13 @@ func getRobots(url string) []string {
    if resperr != nil {
       fmt.Println("\033[31m%s \033[0m\n", resperr)
    } else {
-      status_code := resp.StatusCode
-
-      // 401 unauthorized
-      if status_code == 401{
-
       // 200 Found
-      } else if status_code == 200{
+      } if resp.StatusCode == 200 {
 
-      // 403 Forbidden
-      } else if status_code == 403{
-
+         // TODO
+         // parse robots.txt
       }
+
 
 
 
